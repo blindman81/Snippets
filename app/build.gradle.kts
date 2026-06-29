2fc11fa142c1a9cc3da1ed22f23d712461fc08b5
blindman81 <narayanafm1@gmail.com>
2026-06-29 08:33:34 +0530

    2.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.ln.android.snippets"
         minSdk = 24
         targetSdk = 37
-        versionCode = 16
-        versionName = "2.0"
+        versionCode = 17
+        versionName = "2.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
